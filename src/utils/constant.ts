14909438aae683c778d04468e10e3b73c2c2db57
1hiteshk <1hiteshk@gmail.com>
2025-01-13 12:57:22 +0530

    made base url dynamic

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,4 +1,11 @@
-export const BASE_URL = `/api/api`;
+// export const BASE_URL = `http://localhost:7777/api`; // for dev. mode to login sign up
+// export const BASE_URL = `/api/api`; // for production
+/* export const BASE_URL =
+import.meta.env.MODE === "production"
+    ? `/api/api` // Production URL
+    : `http://localhost:7777/api`; // Development URL */
+
+export const BASE_URL = location.hostname==='localhost'? 'http://localhost:7777/api' : '/api/api'; 
 
  // List of form fields to map over
 export const formFields = [
